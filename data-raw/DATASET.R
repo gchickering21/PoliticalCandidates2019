20755fed71469b22679ed4878fcae8845bbecbcf
Graham Chickering <gchickering21@amherst.edu>
2020-11-06 15:11:25 -0500

    Updating and fixing dataset itself

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,7 +5,17 @@ PoliticalCandidates2019 <- readr::read_csv("candidates_rd2019.csv") %>%
     candidate_id = candidate_uuid,
     party_roll_up = party_id_roll_up
   ) %>%
-  select(candidate_id, state, office_level, office_role,
-         electoral_district, office_name, number_elected, candidate_name,
-         candidate_party, party_roll_up, race, gender, winner_y_n)
+  select(
+    candidate_id, state, office_level, office_role,
+    electoral_district, office_name, number_elected, candidate_name,
+    candidate_party, party_roll_up, race, gender, winner_y_n
+  ) %>%
+  mutate(
+    race = tolower(race), gender = tolower(gender), candidate_party = tolower(candidate_party),
+    race = gsub("african-american", "african american", race),
+    party_roll_up = gsub("Democratic Party", "Democratic", party_roll_up, ignore.case = TRUE),
+    party_roll_up = gsub("Republican Party", "Republican", party_roll_up, ignore.case = TRUE)
+  )
+
 usethis::use_data(PoliticalCandidates2019)
+
